e5aaa21260a13f60780b4ce0bfec8fb448cd1f26
James Glover <james.glover7@outlook.com>
2025-11-12 23:34:38 +0000

    Commented out test dependency

--- lazy.lua
+++ lazy.lua
@@ -4,7 +4,7 @@ return {
 		'nvim-java/lua-async-await',
 		'nvim-java/nvim-java-refactor',
 		'nvim-java/nvim-java-core',
-		'nvim-java/nvim-java-test',
+		--'nvim-java/nvim-java-test',
 		'nvim-java/nvim-java-dap',
 		'MunifTanjim/nui.nvim',
 		'neovim/nvim-lspconfig',
